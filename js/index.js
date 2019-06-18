3d4e9406e7403c2322358297345f1753577a11f4
Shelby Diamond <Shelbydiamond518@gmail.com>
2019-06-18 16:01:53 -0700

    5/10 done

--- js/index.js
+++ js/index.js
@@ -10,14 +10,42 @@ funBus.addEventListener("click", event => {
 // ------------------ Changes all h2 tags to troll if you click on them: ---------------
 const h2Tag = document.querySelectorAll(".container h2");
 h2Tag.forEach(h2 => {
-  h2.addEventListener("click", event => {
-    h2.textContent = `Princess Trolling`;
+  h2.addEventListener("click", cv => {
+    h2Tag.forEach(cv => {
+      cv.textContent = `Princess Trolling`;
+    });
   });
 });
 console.log(h2Tag);
 //
-//--------------- NAV BAR: ---------------
-let hide = document.querySelector(".btn");
-hide.addEventListener("mouseenter", function() {
-  this.style.visibility = "hidden";
+//--------------- Button: ---------------
+let hide = document.querySelectorAll(".btn");
+hide.forEach(cv => {
+  cv.addEventListener("mouseenter", function() {
+    this.style.visibility = "hidden";
+  });
 });
+//--------------- COPY: ---------------
+let hahaha = document.querySelectorAll("p");
+hahaha.forEach(cv => {
+  cv.addEventListener("copy", function() {
+    hahaha.forEach(cv => {
+      cv.textContent = "HAHAHA SUCKER!";
+      cv.style.color = "blue";
+    });
+  });
+});
+
+//----------- Mouseover: -----
+
+let content = document.querySelector(".text-content");
+content.addEventListener(
+  "mouseover",
+  function(event) {
+    event.target.style.color = "purple";
+    setTimeout(function() {
+      event.target.style.color = "";
+    }, 1000);
+  },
+  false
+);
