54909058b577c6ebbe5ac15d1dce3901d4306ca1
Shelby Diamond <Shelbydiamond518@gmail.com>
2019-06-18 13:55:26 -0700

    updated 1/10 events

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+
+
+// ------ Changes the fun bus in the Nav to saying Princess Trolling
+const funBus = document.querySelector(".logo-heading")
+
+funBus.addEventListener('click', event => {
+    funBus.textContent = `Princess Trolling`
+} );
