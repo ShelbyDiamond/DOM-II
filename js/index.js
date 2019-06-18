d4f3ae38b628d71d347343fac51e72cdd62b844a
Shelby Diamond <Shelbydiamond518@gmail.com>
2019-06-18 15:22:44 -0700

    updated 2/10

--- js/index.js
+++ js/index.js
@@ -1,9 +1,18 @@
 // Your code goes here
 
+// --------------- Changes the fun bus in the Nav to saying Princess Trolling: ---------------
+const funBus = document.querySelector(".logo-heading");
 
-// ------ Changes the fun bus in the Nav to saying Princess Trolling
-const funBus = document.querySelector(".logo-heading")
+funBus.addEventListener("click", event => {
+  funBus.textContent = `Shelby's Princess Website`;
+});
 
-funBus.addEventListener('click', event => {
-    funBus.textContent = `Princess Trolling`
-} );
+// ------------------ Changes all h2 tags to troll if you click on them: ---------------
+const h2Tag = document.querySelectorAll(".container h2");
+h2Tag.forEach(h2 => {
+  h2.addEventListener("click", event => {
+    h2.textContent = `Princess Trolling`;
+  });
+});
+console.log(h2Tag);
+//
